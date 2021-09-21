152afc9272f664fc763f94de2bb03a8b7a9356c8
Chris Troutner <chris.troutner@gmail.com>
2021-09-21 11:47:02 -0700

    Disabled ipfs and ipfs-coord startup stuff for now

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -5,37 +5,37 @@
 */
 
 // Public NPM libraries
-const BCHJS = require("@psf/bch-js");
+const BCHJS = require('@psf/bch-js')
 
 // Load individual adapter libraries.
-const IPFSAdapter = require("./ipfs");
-const LocalDB = require("./localdb");
-const LogsAPI = require("./logapi");
-const Passport = require("./passport");
-const Nodemailer = require("./nodemailer");
+const IPFSAdapter = require('./ipfs')
+const LocalDB = require('./localdb')
+const LogsAPI = require('./logapi')
+const Passport = require('./passport')
+const Nodemailer = require('./nodemailer')
 // const { wlogger } = require('./wlogger')
-const JSONFiles = require("./json-files");
-const FullStackJWT = require("./fullstack-jwt");
+const JSONFiles = require('./json-files')
+const FullStackJWT = require('./fullstack-jwt')
 
-const config = require("../../config");
+const config = require('../../config')
 
 class Adapters {
-  constructor(localConfig = {}) {
+  constructor (localConfig = {}) {
     // Encapsulate dependencies
-    this.ipfs = new IPFSAdapter();
-    this.localdb = new LocalDB();
-    this.logapi = new LogsAPI();
-    this.passport = new Passport();
-    this.nodemailer = new Nodemailer();
-    this.jsonFiles = new JSONFiles();
-    this.bchjs = new BCHJS();
-    this.config = config;
+    this.ipfs = new IPFSAdapter()
+    this.localdb = new LocalDB()
+    this.logapi = new LogsAPI()
+    this.passport = new Passport()
+    this.nodemailer = new Nodemailer()
+    this.jsonFiles = new JSONFiles()
+    this.bchjs = new BCHJS()
+    this.config = config
 
     // Get a valid JWT API key and instance bch-js.
-    this.fullStackJwt = new FullStackJWT(config);
+    this.fullStackJwt = new FullStackJWT(config)
   }
 
-  async start() {
+  async start () {
     try {
       // if (this.config.getJwtAtStartup) {
       //   // Get a JWT token and instantiate bch-js with it. Then pass that instance
@@ -48,10 +48,10 @@ class Adapters {
       // // Start the IPFS node.
       // await this.ipfs.start()
     } catch (err) {
-      console.error("Error in adapters/index.js/start()");
-      throw err;
+      console.error('Error in adapters/index.js/start()')
+      throw err
     }
   }
 }
 
-module.exports = Adapters;
+module.exports = Adapters
